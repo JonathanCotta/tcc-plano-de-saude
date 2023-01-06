fef07bd69f651cdd8de249a56648853f2fdfe53d
Jonathan Silva <jonathan.cotta_dextra@prestador.globo>
2023-01-06 16:29:26 -0300

    breadcrumbs refactor

--- src/components/@extended/Breadcrumbs.js
+++ src/components/@extended/Breadcrumbs.js
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
@@ -11,119 +10,61 @@ import MainCard from '../MainCard';
 
 // ==============================|| BREADCRUMBS ||============================== //
 
-const Breadcrumbs = ({ navigation, title, ...others }) => {
+const Breadcrumbs = () => {
     const location = useLocation();
-    const [main, setMain] = useState();
-    const [item, setItem] = useState();
+    const [splitedLocation, setSplitedLocation] = useState([]);
 
-    // set active item state
-    const getCollapse = (menu) => {
-        if (menu.children) {
-            menu.children.filter((collapse) => {
-                if (collapse.type && collapse.type === 'collapse') {
-                    getCollapse(collapse);
-                } else if (collapse.type && collapse.type === 'item') {
-                    if (location.pathname === collapse.url) {
-                        setMain(menu);
-                        setItem(collapse);
-                    }
-                }
-                return false;
-            });
-        }
-    };
+    const capitalized = (word = '') => word.charAt(0).toUpperCase() + word.slice(1);
 
     useEffect(() => {
-        navigation?.items?.map((menu) => {
-            if (menu.type && menu.type === 'group') {
-                getCollapse(menu);
-            }
-            return false;
-        });
-    });
-
-    // only used for component demo breadcrumbs
-    if (location.pathname === '/breadcrumbs') {
-        location.pathname = '/dashboard/analytics';
-    }
-
-    let mainContent;
-    let itemContent;
-    let breadcrumbContent = <Typography />;
-    let itemTitle = '';
+        const locationArray = location.pathname.split('/').filter((i) => i !== '');
+        setSplitedLocation(locationArray);
+    }, [location]);
 
     // collapse item
-    if (main && main.type === 'collapse') {
-        mainContent = (
-            <Typography
-                component={Link}
-                to={document.location.pathname}
-                variant="h6"
-                sx={{ textDecoration: 'none' }}
-                color="textSecondary"
-            >
-                {main.title}
-            </Typography>
-        );
-    }
+    const mainContent = (
+        <Typography
+            component={Link}
+            to={splitedLocation[1] ? `/${splitedLocation[0]}s` : `/${splitedLocation[0]}`}
+            variant="h6"
+            sx={{ textDecoration: 'none' }}
+            color="textSecondary"
+        >
+            {capitalized(splitedLocation[0])}
+        </Typography>
+    );
 
     // items
-    if (item && item.type === 'item') {
-        itemTitle = item.title;
-        itemContent = (
-            <Typography variant="subtitle1" color="textPrimary">
-                {itemTitle}
-            </Typography>
-        );
+    const itemContent = splitedLocation[1] && (
+        <Typography variant="subtitle1" color="textPrimary">
+            {capitalized(splitedLocation[1])}
+        </Typography>
+    );
 
-        // main
-        if (item.breadcrumbs !== false) {
-            breadcrumbContent = (
-                <MainCard
-                    border={false}
-                    sx={{ mb: 3, bgcolor: 'transparent' }}
-                    {...others}
-                    content={false}
-                >
-                    <Grid
-                        container
-                        direction="column"
-                        justifyContent="flex-start"
-                        alignItems="flex-start"
-                        spacing={1}
-                    >
-                        <Grid item>
-                            <MuiBreadcrumbs aria-label="breadcrumb">
-                                <Typography
-                                    component={Link}
-                                    to="/"
-                                    color="textSecondary"
-                                    variant="h6"
-                                    sx={{ textDecoration: 'none' }}
-                                >
-                                    Home
-                                </Typography>
-                                {mainContent}
-                                {itemContent}
-                            </MuiBreadcrumbs>
-                        </Grid>
-                        {title && (
-                            <Grid item sx={{ mt: 2 }}>
-                                <Typography variant="h5">{item.title}</Typography>
-                            </Grid>
-                        )}
-                    </Grid>
-                </MainCard>
-            );
-        }
-    }
+    // main
+    return (
+        <MainCard border={false} sx={{ mb: 3, bgcolor: 'transparent' }} content={false}>
+            <Grid container direction="column" justifyContent="flex-start" alignItems="flex-start">
+                <Grid item>
+                    <MuiBreadcrumbs aria-label="breadcrumb">
+                        <Typography
+                            component={Link}
+                            to="/"
+                            color="textSecondary"
+                            variant="h6"
+                            sx={{ textDecoration: 'none' }}
+                        >
+                            Home
+                        </Typography>
+                        {mainContent}
+                        {itemContent}
+                    </MuiBreadcrumbs>
+                </Grid>
+            </Grid>
+        </MainCard>
+    );
 
     return breadcrumbContent;
-};
-
-Breadcrumbs.propTypes = {
-    navigation: PropTypes.object,
-    title: PropTypes.bool
 };
 
 export default Breadcrumbs;
